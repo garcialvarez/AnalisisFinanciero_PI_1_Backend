1b235ceb9696c99b229f1f814585a85e7c221298
eospgonz10 <estiven.ospinag@udea.edu.co>
2025-09-09 20:40:17 -0500

    Se agrega proyecto base de springboot Java 17

---
+++ src/test/java/com/example/AnalisisFinanciero_back/AnalisisFinancieroBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AnalisisFinanciero_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalisisFinancieroBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
